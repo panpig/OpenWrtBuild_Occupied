599d3acea2ff7c07bc39f6f9a20f3244047fcc17
panpig <89371886+panpig@users.noreply.github.com>
2023-01-17 02:26:57 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -10,8 +10,21 @@
 # 修改默认IP
 # sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
 
-# Autocore
+# Autocoresed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
+
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=1000000' package/base-files/files/etc/sysctl.conf
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+sed -i '/customized in this file/a net.core.somaxconn=65536' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.ipv4.udp_wmem_min=6553500' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.ipv4.udp_rmem_min=6553600' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.core.rmem_max=40960000' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.core.wmem_max=40960000' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.core.wmem_default=40960000' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.core.rmem_default=40960000' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.ipv4.tcp_mem="65535        4638400   64943040"' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.ipv4.udp_mem="65535        4638400   64943040"' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.ipv4.tcp_rmem="65535        4638400   64943040"' package/base-files/files/etc/sysctl.conf
+sed -i '/customized in this file/a net.ipv4.tcp_wmem="65535        4638400   64943040"' package/base-files/files/etc/sysctl.conf
 
 # Cpufreq
 sed -i 's/services/system/g' feeds/luci/applications/luci-app-cpufreq/luasrc/controller/cpufreq.lua
@@ -32,39 +45,20 @@ git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-eqos package/luci-app-eqos
-svn co https://github.com/kiddin9/openwrt-bypass/trunk/luci-app-bypass package/luci-app-bypass
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
-svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman package/luci-app-dockerman
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-smartdns package/luci-app-smartdns
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata package/luci-app-netdata
+
 
 # 科学上网插件依赖
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 # 编译 po2lmo (如果有po2lmo可跳过)
-pushd package/luci-app-openclash/tools/po2lmo
 make && sudo make install
 popd
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/brook
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/chinadns-ng
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/tcping
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/trojan-go
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/trojan-plus
-svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-core package/xray-core
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-plugin package/xray-plugin
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/ssocks
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria package/hysteria
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-plugin package/v2ray-plugin
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-core package/v2ray-core
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2tcp package/dns2tcp
-svn co https://github.com/fw876/helloworld/trunk/v2ray-geodata package/v2ray-geodata
-svn co https://github.com/fw876/helloworld/trunk/naiveproxy package/naiveproxy
 svn co https://github.com/fw876/helloworld/trunk/sagernet-core package/sagernet-core
-svn co https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/shadowsocks-rust
-svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/shadowsocksr-libev
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 svn co https://github.com/fw876/helloworld/trunk/simple-obfs package/simple-obfs
 svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/lua-neturl
 svn co https://github.com/fw876/helloworld/trunk/trojan package/trojan
@@ -120,9 +114,6 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_U
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload\.github\.com/g' {}
 
 # 调整 V2ray服务器 到 VPN 菜单
-sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/controller/*.lua
-sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
-sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
